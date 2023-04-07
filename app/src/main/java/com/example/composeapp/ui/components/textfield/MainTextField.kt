2502fe4ac2a681cb5d510d9356110579d3ba09ff
Guillermo <guillecuesta@gmail.com>
2023-04-07 12:46:21 +0200

    Fix detekt for MainTextField

--- app/src/main/java/com/example/composeapp/ui/components/textfield/MainTextField.kt
+++ app/src/main/java/com/example/composeapp/ui/components/textfield/MainTextField.kt
@@ -2,10 +2,8 @@ package com.example.composeapp.ui.components.textfield
 
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composeapp.ui.theme.primaryTextFieldBackgroundColor
 import com.example.composeapp.ui.theme.primaryTextFieldFocusedBorderColor
