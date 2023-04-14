bef84dcb2cfef9c05eaa1751b41485e7ebf59aa6
Guillermo <guillecuesta@gmail.com>
2023-04-14 11:07:24 +0200

    Fix detekt errors

--- app/src/main/java/com/example/composeapp/ui/components/button/PrimaryButton.kt
+++ app/src/main/java/com/example/composeapp/ui/components/button/PrimaryButton.kt
@@ -4,7 +4,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composeapp.ui.theme.primaryButtonBackground
 import com.example.composeapp.ui.theme.primaryButtonTextColor
---
+++ app/src/main/java/com/example/composeapp/ui/page/SessionPage.kt
@@ -0,0 +1,15 @@
+// package com.example.composeapp.ui.page
+//
+// import androidx.compose.runtime.Composable
+// import androidx.compose.ui.tooling.preview.Preview
+//
+// @Composable
+// fun SessionPage() {
+//
+// }
+//
+// @Preview(showBackground = true)
+// @Composable
+// fun SessionPagePreview() {
+//
+// }
